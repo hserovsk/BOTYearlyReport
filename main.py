70d1f01797b41fb8312aad695e10bb4b33edd8c1
Hubert Serowski R <hserv@protonmail.com>
2023-04-01 11:16:03 +0200

    Exception email added

--- main.py
+++ main.py
@@ -3,14 +3,35 @@ import ssl
 import glob as gb
 import smtplib
 import pandas as pd
+import logging
 import selenium.common.exceptions
 from openpyxl import Workbook
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from dotenv import load_dotenv
 import os
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.action_chains import ActionChains
+from datetime import date
+from pywinauto.application import Application
+
+# Actual Date
+today = date.today()
+date_text_month = today.strftime("%d %B %Y")
+
+# Logger
+logger = logging.getLogger("JobAdvertScraper")
+logging.basicConfig(level=logging.INFO)
+log_filename = 'logfile_{}.log'.format(date_text_month)
+
+
+file_handler = logging.FileHandler(log_filename)
+formatter = logging.Formatter("%(asctime)s: %(levelname)s - %(message)s")
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.INFO)
+logger.addHandler(file_handler)
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 
@@ -21,17 +42,23 @@ chrome_options.add_experimental_option("prefs", {
 browser = webdriver.Chrome(PATH, chrome_options=chrome_options)
 
 def configure():
+    logger.info("###################### SCRAPER STARTING EXECUTION ######################")
+    logger.info("Loading configuration...")
     load_dotenv()
+    logger.info("Loading configuration... Success")
 
 def login_to_web_page():
+    logger.info("Loggin in to ACME website...")
     browser.get("https://acme-test.uipath.com/login")
     browser.maximize_window()
     browser.find_element(By.XPATH, "//div//input[@id='email']").send_keys(f"{os.getenv('ACME_LOGIN')}")
     browser.find_element(By.XPATH, "//div//input[@id='password']").send_keys(f"{os.getenv('ACME_PASSWORD')}")
     browser.find_element(By.XPATH, "//div//button[@type='submit']").click()
     time.sleep(5)
+    logger.info("Loggin in to ACME website... Success")
 
 def download_monthly_report():
+    logger.info("Downloading monthly reports...")
     achains = ActionChains(browser)
     hover_element = browser.find_element(By.XPATH, "//button[normalize-space()='Reports']")
     achains.move_to_element(hover_element).perform()
@@ -49,39 +76,143 @@ def download_monthly_report():
         try:
             select_month.select_by_index(x)
             browser.find_element(By.XPATH, "//button[@id='buttonDownload']").click()
-            time.sleep(3)
+            time.sleep(2)
         except Exception as e:
             try:
                 print(f"{e} try in try except")
             except:
                 print("except in try except")
+    logger.info("Downloading monthly reports... Success")
 def merge_excel_files():
+    logger.info("Merging excel files...")
     path = r"C:\Users\serowskh\PycharmProjects\RPABOTYearlyInovice\downloads"
     filenames = gb.glob(path + r"\xlsx")
     outputxlsx = pd.DataFrame()
 
     for file in filenames:
+        logger.info(f"Merging file {str(file)} ...")
         df = pd.concat(pd.read_excel(file, sheet_name=None,), ignore_index=True, sort=False)
         outputxlsx = outputxlsx.append(df, ignore_index=True)
     outputxlsx.to_excel(r"C:\Users\serowskh\PycharmProjects\RPABOTYearlyInovice\mergedFiles\mergedfile.xlsx")
+    logger.info("Merging excel files... Success")
 
 def redirect_to_work_items():
-    browser.click_button("xpath://div//button[@class='btn btn-default btn-lg']")
+    logger.info("Redirecting to work files...")
+    #browser.click_button("xpath://div//button[@class='btn btn-default btn-lg']")
     time.sleep(3)
+    logger.info("Redirecting to work files... Success")
+
 def clean_folders():
+    logger.info("Deleting files in folders...")
     downloaded_files = gb.glob(r"C:\Users\serowskh\PycharmProjects\RPABOTYearlyInovice\downloads\*")
     merged_files = gb.glob(r"C:\Users\serowskh\PycharmProjects\RPABOTYearlyInovice\mergedFiles\*")
     for fd in downloaded_files:
+        logger.info(f"Removing file {str(fd)} in download folder")
         os.remove(fd)
     for fm in merged_files:
+        logger.info(f"Removing file {str(fm)} in merged folder")
         os.remove(fm)
+    logger.info("Deleting files in folders... Success")
+
+def uploading_merged_file():
+    browser.get("https://acme-test.uipath.com")
+    achains = ActionChains(browser)
+    hover_element = browser.find_element(By.XPATH, "//button[normalize-space()='Reports']")
+    achains.move_to_element(hover_element).perform()
+    browser.find_element(By.XPATH, "//a[normalize-space()='Upload Yearly Report']").click()
+    browser.find_element(By.XPATH, "//input[@id='vendorTaxID']").send_keys("IT754893")
+    dropdown_year = browser.find_element(By.XPATH, "//select[@name='reportYear']")
+    select_year = Select(dropdown_year)
+    select_year.select_by_value("2022")
+    time.sleep(10)
+    btitle = browser.title
+    browser.find_element(By.XPATH, "//label[@for='my-file-selector']").click()
+
+    app = Application(backend='uia').connect(title=btitle, timeout=100)
+    #app = Application(backend='uia').connect(title="Open", timeout=100)
+    filebox = app.btitle.child_window(title="File name:", auto_id="1148", control_type="Edit").wrapper_object()
+    #filebox = app.BrowserWindowName.child_window(title="File name:", auto_id="1148", control_type="Edit").wrapper_object()
+    filebox.type_keys(r'C:\Users\serowskh\PycharmProjects\RPABOTYearlyInovice\mergedFiles\mergedfile.xlsx')
+
 def send_mail():
+    smtp_port = 587
+    smtp_server = "smtp.gmail.com"
+
+
+    email_from = f"{os.getenv('GOOGLE_MAIL')}"
+    email_to = f"{os.getenv('GOOGLE_MAIL')}"
+    pswd = f"{os.getenv('GOOGLE_APP_CREDENTIAL')}"
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "multipart test"
+    #message["From"] = f"{os.getenv('GOOGLE_MAIL')}"
+    #message["To"] = f"{os.getenv('GOOGLE_MAIL')}"
+    # write the text/plain part
+    text = f"""\
+    Hi,
+    The bot run was successful
+    Best Regards
+    RPA BOT
+    """
+
+
+    # write the HTML part
+    html = """\
+    <html>
+      <body>
+        <p>Hi,<br>
+           The bot run was successful</p>
+        <p>,Best Regards</p>
+        <p> RPA BOT </p>
+      </body>
+    </html>
+    """
+
+    # convert both parts to MIMEText objects and add them to the MIMEMultipart message
+    part1 = MIMEText(text, "plain")
+    part2 = MIMEText(html, "html")
+    message.attach(part1)
+    message.attach(part2)
+
+    simple_email_context = ssl.create_default_context()
+
+def send_exception_mail(error):
     smtp_port = 587
     smtp_server = "smtp.gmail.com"
     email_from = f"{os.getenv('GOOGLE_MAIL')}"
     email_to = f"{os.getenv('GOOGLE_MAIL')}"
     pswd = f"{os.getenv('GOOGLE_APP_CREDENTIAL')}"
-    message = "RPAHashBot"
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "multipart test"
+    # message["From"] = f"{os.getenv('GOOGLE_MAIL')}"
+    # message["To"] = f"{os.getenv('GOOGLE_MAIL')}"
+    # write the text/plain part
+    text = f"""\
+    Hi,
+    The bot run had following error:
+    ,Best Regards
+    RPA BOT
+    """
+
+    # write the HTML part
+    html = f"""\
+    <html>
+        <body>
+        <p>Hi,<br>
+            The bot run had following error: </p><br>
+            <p> {error} <p>
+        <p>,Best Regards</p>
+        <p> RPA BOT </p>
+        </body>
+    </html>
+    """
+
+    # convert both parts to MIMEText objects and add them to the MIMEMultipart message
+    part1 = MIMEText(text, "plain")
+    part2 = MIMEText(html, "html")
+    message.attach(part1)
+    message.attach(part2)
 
     simple_email_context = ssl.create_default_context()
 
@@ -92,20 +223,27 @@ def send_mail():
         TIE_server.login(email_from, pswd)
         print("Connected to server :) ")
 
-        print(f"Sending email to {email_to}")
-        TIE_server.sendmail(email_from, email_to, message)
-        print(f"Email send successfully to {email_to}")
+        print(f"Sending exception email to {email_to}")
+        TIE_server.sendmail(email_from, email_to, message.as_string())
+        print(f"Email send exception successfully to {email_to}")
     except Exception as e:
         print(e)
     finally:
         TIE_server.quit
+    logger.info("###################### SCRAPER EXECUTION ENDED ######################")
 
 def minimal_task():
-    configure()
-    login_to_web_page()
-    download_monthly_report()
-    merge_excel_files()
-    send_mail()
+    try:
+        configure()
+        login_to_web_page()
+        #download_monthly_report()
+        #merge_excel_files()
+        #uploading_merged_file()
+        #clean_folders()
+        send_mail()
+    except Exception as error:
+        clean_folders()
+        send_exception_mail(error)
 
 if __name__ == "__main__":
     minimal_task()
